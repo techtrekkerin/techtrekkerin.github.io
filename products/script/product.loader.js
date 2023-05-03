80045d11efcf908611227aa4776225bdbd25b584
Abhishek Kumar <techtrekkerin@Abhisheks-MacBook-Air.local>
2023-05-03 13:52:31 +0530

    Bug fixed

--- products/script/product.loader.js
+++ products/script/product.loader.js
@@ -1,8 +1,11 @@
-productsJsonLink = "/products/products/products.json";
+const productsJsonLink = "/products/products/products.json";
+
+categories = undefined;
+products = undefined;
+
 
 window.onload = function() {
     fetchProducts();
-    addLazyLoadListener();
 }
 
 function fetchProducts() {
@@ -10,22 +13,97 @@ function fetchProducts() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var productsJson = JSON.parse(this.responseText);
-            populateProductsInDom(productsJson);
+            var allProductsJson = JSON.parse(this.responseText);
+            products = allProductsJson.products;
+            setAllCategories();
+            populateProductsInDom(undefined);
         }
     };
     xhttp.open("GET", productsJsonLink, true);
     xhttp.send();
 }
 
-function populateProductsInDom(productsJson) {
-    var productRow = document.getElementById("products-row");
-    var productsArray = productsJson.products;
-    for(var j = 0; j < 1; j++) {
-        for(var i = 0; i < productsArray.length; i++) {
-            var product = productsArray[i];
-            productRow.appendChild(getSingleProductDiv(product));
+function setAllCategories() {
+    var categorySet = new Set();
+    for(var i = 0; i < products.length; i++) {
+        categorySet.add(products[i].category);
+    }
+    categories = Array.from(categorySet);
+    addCategoriesToSelect();
+}
+
+function getCapitalizeText(text) {
+    var splitStr = text.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
+}
+
+function addCategoriesToSelect() {
+    var selectElement = document.getElementById("category-selector");
+    selectElement.onchange = function() {
+        var selectedCategory = selectElement.options[selectElement.selectedIndex].value;
+        populateProductsInDom(selectedCategory);
+        //console.log("onchange: ", selectedCategory);
+    }
+
+    var option = document.createElement("option");
+    option.value = "all";
+    option.innerHTML = getCapitalizeText("All Products");
+    selectElement.appendChild(option);
+    
+    categories.sort(function(a,b){return a.localeCompare(b); });
+
+    for(var i = 0; i < categories.length; i++) {
+        var option = document.createElement("option");
+        option.value = categories[i];
+        option.innerHTML = getCapitalizeText(categories[i]);
+        selectElement.appendChild(option);
+    }
+}
+
+function sortByProperty(property){  
+    return function(a,b){  
+       if(a[property] > b[property])  
+          return 1;  
+       else if(a[property] < b[property])  
+          return -1;  
+   
+       return 0;  
+    }  
+ }
+
+ function getProductsArrayByCategory(category) {
+    if(category == undefined || category.localeCompare("all") == 0) {
+        return products;
+    }
+
+    var productsArray = [];
+    for(var i = 0; i < products.length; i++) {
+        var product = products[i];
+        if(product.category.localeCompare(category) == 0) {
+            productsArray.push(product);
         }
+    }
+
+    return productsArray;
+
+ }
+
+function populateProductsInDom(category) {
+    var productRow = document.getElementById("products-row");
+    productRow.innerHTML = "";
+
+    var productsArray = getProductsArrayByCategory(category);
+    productsArray.sort(sortByProperty("priority"));
+
+    for(var i = 0; i < productsArray.length; i++) {
+        var product = productsArray[i];
+        productRow.appendChild(getSingleProductDiv(product));
     }
 }
 
@@ -58,10 +136,4 @@ function getSingleProductDiv(product) {
     mainDiv.appendChild(cardDiv);
 
     return mainDiv;
-}
-
-
-function addLazyLoadListener() {
-   //console.log("event listeners");
-    
 }
