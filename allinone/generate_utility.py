bde7140ee91f475c9cff20ce33f14d4a741aa0e8
Abhishek Kumar <techtrekkerin@Abhisheks-MacBook-Air.local>
2023-05-23 15:14:14 +0530

    night icons added

--- allinone/generate_utility.py
+++ allinone/generate_utility.py
@@ -1,12 +1,13 @@
 import json
 from urllib.parse import urlparse
 
-FILE_NAME = "apps.json"
-TEMP_FILE_NAME = "apps_temp.json"
+FILE_NAME_APPS = "allinone_json/apps.json"
+FILE_NAME_CATEGORIES = "allinone_json/categories.json"
+FILE_NAME_TEMP = "apps_temp.json"
 
 def write_tofile(data_to_write):
 
-    with open(FILE_NAME, "w") as new_file:
+    with open(FILE_NAME_CATEGORIES, "w") as new_file:
         json.dump(data_to_write, new_file, indent=4)
 
 
@@ -31,11 +32,14 @@ def get_connected_domains(url):
     return domains
     
 
+def get_icon_link_night(icon_link):
+    return icon_link.replace("/category_icons/", "/category_icons_night/")
+
 def main():
-    data = get_data_from_file(FILE_NAME)
+    data = get_data_from_file(FILE_NAME_CATEGORIES)
     for key in data.keys():
         app = data[key]
-        app["connected_domains"] = get_connected_domains(app["app_link"])
+        app["icon_link_night"] = get_icon_link_night(app["icon_link"])
         data[key] = app
     
     write_tofile(data)
